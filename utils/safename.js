7bdb74b0de4a1a2b4ab77ee96e356da098084fc4
Patrick Burtchaell <patrick@pburtchaell.com>
2014-07-16 22:17:25 -0500

    Add "utils" folder

---
+++ utils/safename.js
@@ -0,0 +1,11 @@
+module.exports = function (name, patterns) {
+  var prefixes = [
+    'helper', 
+    'handlebars-helper', 
+    'mixin',
+    'assemble'
+  ];
+  var remove = _.unique(_.flatten(_.union([], prefixes, patterns || [])));
+  var re = new RegExp('^(?:' + remove.join('|') + ')[-_]?');
+  return name.replace(re, '').replace(/[\W_]+/g, '_').replace(/^(\d)/, '_$1');
+};
